1c1ff6d63e7b0249e58bf849cf428e28f034be81
Bennett Clews <ben.clews@csiro.au>
2024-06-25 16:09:27 +1000

    refactor: Remove backup Neovim configuration files
    This commit removes the backup Neovim configuration files that were
    previously stored in the `nvim.bak` directory. These files included
    various Lua scripts for configuring plugins and other Neovim settings.
    These files are no longer needed as the configuration has been moved
    to a different location.

--- nvim.bak/lua/vim-options.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd("set number")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set clipboard=unnamedplus")
-vim.g.mapleader = " "
