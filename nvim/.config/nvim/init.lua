0f425acb6f9d342cb4d88f1559601a6ef2575d31
Bennett Clews <ben.clews@csiro.au>
2024-09-17 22:28:18 +1000

    fix(nvim): ENOENT: no such file or directory
    As per:
    https://github.com/neovim/neovim/issues/28058#issuecomment-2146978107

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- https://github.com/neovim/neovim/issues/28058#issuecomment-2146978107
+local lsp = vim.lsp
+
+local make_client_capabilities = lsp.protocol.make_client_capabilities
+
+function lsp.protocol.make_client_capabilities()
+  local caps = make_client_capabilities()
+  if not (caps.workspace or {}).didChangeWatchedFiles then
+    vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
+  else
+    caps.workspace.didChangeWatchedFiles = nil
+  end
+
+  return caps
+end
